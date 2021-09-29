5611dee50e1d4af23709e88693cc22f787d4219a
faaaasszaa <gilcruzada16@gmail.com>
2021-09-29 15:53:19 +0800

    fixed syntax issues, added comments

--- Selection Sort/selectionSort.py
+++ Selection Sort/selectionSort.py
@@ -1,24 +1,32 @@
+# Function to return the smallest index
 def findSmallest(arr):
+    # Starts the first index of the array
     smallest = arr[0]
+    # Createing a variable to store the index of the smallest element
     smallIndex = 0
-
+    # Loops through the array based on its length
     for i in range(1, len(arr)):
+        # If the current index is smaller than stored small index, we change values accordingy
         if arr[i] < smallest:
             smallest  = arr[i]
             smallIndex = i
-        return smallIndex
+    # We return the index of the smallest element
+    return smallIndex
 
+# Selection Sort Array
 def selectionSort(arr):
-    sorted_array = []
+    # Creating an array to store from least go greatest
+    newArr = []
 
-    for i in range(1, len(arr)):
+    for i in range(0, len(arr)):
         smallElement = findSmallest(arr)
-        sorted_array.append(arr.pop(smallElement))
-    return sorted_array
+        # We find the smallest element then add it to the new array
+        newArr.append(arr.pop(smallElement))
+    return newArr
 
-the_list = [3, 2, 6, 8, 9, 1, 10, 13, 12]
+the_list = [7, 6, 3, 4, 6, 8, 9, 0, 1]
 
-print(findSmallest(the_list))
+print("The index of the smallest element: " + str(findSmallest(the_list)))
 print(selectionSort(the_list))
 
 
